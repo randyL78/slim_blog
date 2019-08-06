8b5bfeb7a130b6040ab295c54e2feeabddbdb576
Randy's Mac <randydavidl78@gmail.com>
2019-08-06 07:55:41 -0400

    Create static twig views

--- src/dependencies.php
+++ src/dependencies.php
@@ -5,20 +5,15 @@ use Slim\App;
 return function (App $app) {
     $container = $app->getContainer();
 
-    // view renderer
-    $container['renderer'] = function ($c) {
-        $settings = $c->get('settings')['renderer'];
-        return new \Slim\Views\PhpRenderer($settings['template_path']);
-    };
-
-    // Register component on container
-    $container['view'] = function ($container) {
-        $view = new \Slim\Views\Twig(__DIR__ .  '/../templates', [
-            'cache' => false
+    // Register twig view component on container
+    $container['view'] = function ($c) {
+        $settings = $c->get('settings')['view'];
+        $view = new \Slim\Views\Twig($settings['template_path'], [
+            'cache' => $settings['cache']
         ]);
 
         // Instantiate and add Slim specific extension
-        $router = $container->get('router');
+        $router = $c->get('router');
         $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
         $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
 
--- src/routes.php
+++ src/routes.php
@@ -7,13 +7,45 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
-        // Sample log message
+    // home route
+    $app->get('/', function (Request $request, Response $response, array $args) use ($container) {
+        // Log message
         $container->get('logger')->info("Slim-Skeleton '/' route");
 
-        // Render index view
-        // return $container->get('renderer')->render($response, 'index.phtml', $args);
-
+        // Render the home page
         return $container->get('view')->render($response, 'home.twig', $args);
+    });
+
+    // new route
+    // * ensure this route is checked before details route
+    $app->get('/blogs/new', function (Request $request, Response $response, array $args) use ($container) {
+        // Log message
+        $container->get('logger')->info("Slim-Skeleton '/blogs/new' route");
+
+        $args['title'] = 'Publish New Entry';
+
+        // render new blog post form
+        return $container->get('view')->render($response, 'new.twig', $args);
+    });
+
+    // blog details route
+    $app->get('/blogs/{id}', function (Request $request, Response $response, array $args) use ($container) {
+        // Log message
+        $container->get('logger')->info("Slim-Skeleton '/blogs/id' route");
+
+        // render new blog post form
+        return $container->get('view')->render($response, 'detail.twig', $args);
+    });
+
+
+    // edit blog route
+    $app->get('/blogs/{id}/edit', function (Request $request, Response $response, array $args) use ($container) {
+        // Log message
+        $container->get('logger')->info("Slim-Skeleton '/blogs/id/edit' route");
+
+        $args['title'] = 'Edit Entry';
+
+        // render new blog post form
+        return $container->get('view')->render($response, 'new.twig', $args);
     });
 };
