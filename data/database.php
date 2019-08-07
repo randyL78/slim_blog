15522e304c4fa67954b323ef9e223743289c7c14
Randy's Mac <randydavidl78@gmail.com>
2019-08-07 08:57:27 -0400

    Connect db to home view

--- data/database.php
+++ data/database.php
@@ -9,24 +9,24 @@
  */
 abstract class Database
 {
-  /**
-   * Get all posts from the posts table. 
-   * Limit the columns to the title, date, and slug for easy
-   * list displaying.
-   * @param PDO $db an object representing the database connection
-   * @return mixed array containing the posts
-   */
-  public static function getStaticPosts(PDO $db)
-  {
-    $sql = 'SELECT title, date, slug FROM posts';
+    /**
+     * Get all posts from the posts table. 
+     * Limit the columns to the title, date, and slug for easy
+     * list displaying.
+     * @param PDO $db an object representing the database connection
+     * @return mixed array containing the posts
+     */
+    public static function getStaticPosts(PDO $db)
+    {
+        $sql = 'SELECT title, date, slug FROM posts';
 
-    try {
-      $results = $db->query($sql);
-    } catch (Exception $ex) {
-      echo "Bad query";
-      exit;
+        try {
+            $results = $db->query($sql);
+        } catch (Exception $ex) {
+            echo "Bad query";
+            exit;
+        }
+
+        return $results->fetchAll();
     }
-
-    return $results->fetchAll();
-  }
 }
